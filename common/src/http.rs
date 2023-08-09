a7173b285eab6cad864c15f43358aba17728aaff
Maciej Zwoliński <mac.zwolinski@gmail.com>
2023-08-09 19:55:34 +0200

    feat(icp): add redirections on http requests (#7)

--- common/src/http.rs
+++ common/src/http.rs
@@ -21,14 +21,48 @@ mod icp {
 
     use super::HttpResponse;
 
+    // TODO: should this be configurable?
+    const MAX_REDIRECTS: usize = 2;
+
     pub async fn get(url: &str) -> Result<HttpResponse, HttpError> {
-        let req = CanisterHttpRequestArgument {
-            url: url.to_owned(),
-            method: HttpMethod::GET,
-            ..Default::default()
-        };
-        let resp = http_request(req, DEFAULT_HTTP_OUTCALL_COST).await?;
-        resp.0.try_into()
+        let mut url = url.to_owned();
+
+        for _ in 0..=MAX_REDIRECTS {
+            let req = CanisterHttpRequestArgument {
+                url: url.to_owned(),
+                method: HttpMethod::GET,
+                ..Default::default()
+            };
+            let resp = http_request(req, DEFAULT_HTTP_OUTCALL_COST).await?.0;
+
+            match http_status_from_nat(&resp.status)? {
+                // moved_permamently / found / see_other / temporary_redirect / permament_redirect
+                // redirect and retry
+                301..=303 | 307..=308 => {
+                    url = resp
+                        .headers
+                        .iter()
+                        .find_map(|header| {
+                            if header.name.eq_ignore_ascii_case("location") {
+                                Some(header.value.clone())
+                            } else {
+                                None
+                            }
+                        })
+                        .ok_or_else(|| {
+                            HttpError::Other(format!(
+                                "Got {} status code without 'Location' header",
+                                resp.status
+                            ))
+                        })?;
+                }
+                _ => return resp.try_into(),
+            }
+        }
+
+        Err(HttpError::Other(
+            "Exceeded redirection limit: {MAX_REDIRECTS}".to_owned(),
+        ))
     }
 
     pub async fn post(
@@ -58,15 +92,17 @@ mod icp {
         type Error = HttpError;
 
         fn try_from(value: CanisterHttpResponse) -> Result<Self, Self::Error> {
-            let status = value.status.0.try_into().map_err(|err| {
-                HttpError::Other(format!("Status should be a 3 digit number, got: {err}"))
-            })?;
-
             Ok(Self {
-                status,
+                status: http_status_from_nat(&value.status)?,
                 body: value.body,
             })
         }
+    }
+
+    fn http_status_from_nat(status: &candid::Nat) -> Result<u16, HttpError> {
+        (&status.0).try_into().map_err(|err| {
+            HttpError::Other(format!("Status should be a 3 digit number, got: {err}"))
+        })
     }
 }
 
